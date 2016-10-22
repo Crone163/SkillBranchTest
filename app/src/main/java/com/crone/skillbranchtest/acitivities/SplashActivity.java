1da138de20bf2f9e6a71651fe8995b21c8b2510b
Crone <defrag163@mail.ru>
2016-10-22 16:28:03 +0300

    Using EventBus

--- app/src/main/java/com/crone/skillbranchtest/acitivities/SplashActivity.java
+++ app/src/main/java/com/crone/skillbranchtest/acitivities/SplashActivity.java
@@ -3,15 +3,11 @@ package com.crone.skillbranchtest.acitivities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Parcel;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
+
 import android.support.v7.app.AppCompatActivity;
-import android.view.WindowManager;
-import android.widget.ImageView;
 
 import com.crone.skillbranchtest.R;
 import com.crone.skillbranchtest.data.DataManager;
@@ -26,10 +22,10 @@ import com.crone.skillbranchtest.network.models.HousesModelRes;
 import com.crone.skillbranchtest.network.models.PersonsModelRes;
 import com.crone.skillbranchtest.utils.MyConfig;
 import com.crone.skillbranchtest.utils.NetworkStatusChecker;
-import com.squareup.picasso.Picasso;
+
 
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.greendao.query.WhereCondition;
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -65,20 +61,15 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
+
+
 
         checkSessions = 0;
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.splash_coordinator);
         mDataManager = DataManager.getInstance();
 
-        //Full Screen
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
-        ImageView splashImage = (ImageView) findViewById(R.id.splashImage);
-
-        Picasso.with(this).load(R.drawable.background_splash).fit().centerCrop().into(splashImage);
 
         mSharedPreferences = getPreferences(MODE_PRIVATE);
 
